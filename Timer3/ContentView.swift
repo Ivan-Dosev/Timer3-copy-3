241a9a0a3ff190f671c595188ef44d8708d825bb
Ivan Dosev <biobeehive@abv.bg>
2020-09-19 12:56:54 +0300

    Initial Commit

---
+++ Timer3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timer3
+//
+//  Created by Ivan Dimitrov on 19.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timer3/Timer3App.swift
@@ -0,0 +1,17 @@
+//
+//  Timer3App.swift
+//  Timer3
+//
+//  Created by Ivan Dimitrov on 19.09.20.
+//
+
+import SwiftUI
+
+@main
+struct Timer3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
